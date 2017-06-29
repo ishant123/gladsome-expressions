bb46caaecbd7b1aaa710c3852f5f34c2dfbefcd9
Ishant <ishant@nagarro.com>
2017-06-29 14:26:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+/**
+ * Created by ishant on 28-06-2017.
+ */
+
+var express = require('express');
+var app = express();
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+app.listen(port,function(err){
+   if(err){
+       console.log("There is some error with port listen");
+   }
+    else{
+       console.log("Application is running on port "+ port);
+   }
+});
